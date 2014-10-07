3c046ca54399326853633c4d4beb5a06203bfe78
Daniel Burnette <dcburnette999@gmail.com>
2014-10-06 21:39:28 -0500

    learned about and implemented MVC pattern and also about localStorage in javascript

--- quizzy_controllers.js
+++ quizzy_controllers.js
@@ -2,21 +2,50 @@ var ApplicationController = {
 	$container: undefined,
 	questionViews: [],
 	questionModels: [],
+
+	questionAvgScores: 
+	{
+		question1: {timesPlayed: undefined, avg: undefined},
+		question2: {timesPlayed: undefined, avg: undefined},
+		question3: {timesPlayed: undefined, avg: undefined},
+		question4: {timesPlayed: undefined, avg: undefined},
+		question5: {timesPlayed: undefined, avg: undefined},
+		question6: {timesPlayed: undefined, avg: undefined},
+	},
+
 	totalQuestions: undefined,
 	correctAnswers: 0,
 	highScore: 0,
 	startQuiz: function($container) {
 
+		this.$container = $container;
+		var questionsData = quizData.questions;
+		this.totalQuestions = questionsData.length;
+
+		//High Score logic initiation
 		if (localStorage['highScore'] != undefined) {
-			this.highScore = localStorage['highScore']
+			this.highScore = localStorage['highScore'];
 		}
 		else {
 			localStorage['highScore'] = 0;
 		}
 
-		this.$container = $container;
-		var questionsData = quizData.questions;
-		this.totalQuestions = questionsData.length;
+		//Question average score logic initiation
+		for (var i=1; i < this.totalQuestions + 1; i++){
+			timesPlayed = localStorage['timesPlayed' + i];
+			avg = localStorage['avg' + i];
+			if (timesPlayed == undefined){
+				timesPlayed = 0;
+			    localStorage['timesPlayed' + i] = timesPlayed;
+			} 
+			if (avg == undefined){
+				avg = 0;
+				localStorage['avg' + i] = avg;
+			}
+		}
+		
+
+
 		for (var i = 0; i < questionsData.length; i++) {
 			var questionModel = new Question(questionsData[i]);
 			var questionView = new QuestionView(questionModel, this.$container);
@@ -39,11 +68,19 @@ var ApplicationController = {
 	},
 
 	checkAnswer: function(userAnswer) {
-		console.log(userAnswer);
 		var currentQuestion = this.questionModels[this.currentQuestionIndex];
 		var answer = currentQuestion.answer;
+		var oldCorrectAnswers = this.correctAnswers;
 		if (userAnswer === answer) {
 			this.correctAnswers++;
+		}
+
+		// So that we know how to update the average!
+		if (this.correctAnswers == oldCorrectAnswers + 1){
+			this.updateQuestionAverage(this.currentQuestionIndex, true);
+		}
+		else {
+			this.updateQuestionAverage(this.currentQuestionIndex, false);
 		}
 	},
 
@@ -61,6 +98,23 @@ var ApplicationController = {
 		}
 	},
 
+	updateQuestionAverage: function(index, isCorrect){
+		var index = index+1;
+		var oldAvg = parseInt(localStorage['avg'+ index]);
+		var oldTimesPlayed = parseInt(localStorage['timesPlayed' + index]);
+		var oldTotal = oldAvg * oldTimesPlayed;
+		var timesPlayed = parseInt(localStorage['timesPlayed' + index]) + 1;
+		if (isCorrect == true){
+			var newTotal = oldTotal+1;
+			localStorage['avg' + index] = (newTotal)/timesPlayed;
+		}
+		else {
+			localStorage['avg' + index] = (oldTotal)/timesPlayed;
+		}
+
+		localStorage['timesPlayed' + index] = timesPlayed;
+	},
+
 	showResults: function(percentScore) {
 		this.hideQuestions();
 
@@ -68,10 +122,14 @@ var ApplicationController = {
 		if (this.highScore > localStorage['highScore']) {
 			localStorage['highScore'] = this.highScore
 		}
+
 		this.$container.append(
 			'<h1>You got ' + percentScore + '%! That\'s ' +
 			this.correctAnswers + ' out of ' + this.totalQuestions + '!!!!!!!' +
-			"The current high score is " + this.highScore
+			"The current high score is " + this.highScore + " Question 1's average is: " + localStorage['avg1'] +
+			" Question 2's average is: " + localStorage['avg2'] + " Question 3's average is: " + localStorage['avg3'] +
+			" Question 4's average is: " + localStorage['avg4'] + " Question 5's average is: " + localStorage['avg5'] +
+			" Question 6's average is: " + localStorage['avg6']
 		);
 	}
 };
--- quizzy_views.js
+++ quizzy_views.js
@@ -2,7 +2,6 @@ function QuestionView(questionModel, $container) {
 	var myTemplate = _.template([
 		'<div>',
 			'<h2><%= question %></h2>',
-			'<p>the answer is <%= answer %></p>',
 			'<% for (var i = 0; i < choices.length; i++) { %>',
 				'<input name="<%= id %>" type="radio" value="<%= choices[i] %>">',
 				'<label><%= choices[i] %></label>',
@@ -12,7 +11,6 @@ function QuestionView(questionModel, $container) {
 	].join(''));
 	var compiledHTML = myTemplate({
 		question: questionModel.question,
-		answer: questionModel.answer,
 		choices: questionModel.choices,
 		id: questionModel.id
 	});
